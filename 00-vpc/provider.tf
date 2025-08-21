e97fa154e3761ddc9e1672569bb494db33c8609e
prasad <prasad@gmail.com>
2025-08-21 15:33:51 +0530

    project

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "prasad-remote-state"
+    bucket = "prasad-terraform-remote-state"
     key    = "expense-dev-vpc"
     region = "us-east-1"
     dynamodb_table = "eks-infra-locking"
